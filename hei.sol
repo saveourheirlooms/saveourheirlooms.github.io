813db1761313e649a7cec9c8f97f057dc5be2b3a
Ümit Kadiroğlu <38588465+umitkadiroglu@users.noreply.github.com>
2022-06-23 13:39:53 +0300

    Create hei.sol

---
+++ hei.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract hei is ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl {
+    using Counters for Counters.Counter;
+    mapping(string => uint8) hashes;
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Heirloom", "HEI") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+    }
+
+    function safeMint(address to) public {
+        require(hasRole(MINTER_ROLE, msg.sender));
+        _safeMint(to, _tokenIdCounter.current());
+        _tokenIdCounter.increment();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+    
+    function mintItem(address recipient, string memory hash, string memory metadata)
+      public
+      returns (uint256)
+      {
+        require(hasRole(MINTER_ROLE, msg.sender));
+        require(hashes[hash] != 1);
+        hashes[hash] = 1;
+        _tokenIdCounter.increment();
+        uint256 newItemId = _tokenIdCounter.current();
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, metadata);
+        return newItemId;
+      }
+      
+      function buyToken(uint256 _tokenId) public payable {
+        require(msg.sender != address(0));
+        require(_exists(_tokenId));
+        address tokenOwner = ownerOf(_tokenId);
+        require(tokenOwner != address(0));
+        require(tokenOwner != msg.sender);
+        _transfer(tokenOwner, msg.sender, _tokenId);
+
+      }
+}
